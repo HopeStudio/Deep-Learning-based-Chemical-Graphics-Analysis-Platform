417fde694fade300ae65cb576fa8894864268ae6
everbrez <everbrez@gmail.com>
2020-01-18 19:37:29 +0800

    :dizzy: feature (models) export the model

--- deep-learning-app/src/models/functionalgroup.py
+++ deep-learning-app/src/models/functionalgroup.py
@@ -5,6 +5,7 @@ from tensorflow import keras
 from tensorflow.keras import datasets, layers, models, regularizers
 import random
 import numpy as np
+import datetime
 # import matplotlib.pyplot as plt
 from load_data import load_data
 
@@ -54,11 +55,11 @@ def evaludate_full_connected_model(model):
 
 def create_conv_model(input_num=input_num, addtion_num=input_num):
   inputs = keras.Input(shape=(input_num,), name='main')
-  addition_inputs = keras.Input(shape=(addtion_num,), name='addition')
+  # addition_inputs = keras.Input(shape=(addtion_num,), name='addition')
 
   x = inputs
   x = layers.Reshape(target_shape=(1, input_num))(x)
-  x = layers.Conv1D(100, 15, activation='relu', strides=1, padding='same')(x)
+  x = layers.Conv1D(50, 10, activation='relu', strides=1, padding='same')(x)
   x = layers.Conv1D(50, 10, activation='relu', strides=1, padding='same')(x)
   x = layers.MaxPooling1D(5, padding='same')(x)
   x = layers.Conv1D(50, 5, activation='relu', strides=1, padding='same')(x)
@@ -66,41 +67,51 @@ def create_conv_model(input_num=input_num, addtion_num=input_num):
   x = layers.Flatten()(x)
 
   # conbine with additon_inputs
-  x = layers.Concatenate()([x, addition_inputs])
+  # x = layers.Concatenate()([x, addition_inputs])
+  x = layers.Concatenate()([x, inputs])
 
   x = layers.Dense(200, activation='relu')(x)
   x = layers.Dropout(0.2)(x)
   outputs = layers.Dense(output_num, activation='sigmoid')(x)
 
-  model = keras.Model(inputs=[inputs, addition_inputs], outputs=outputs, name='functional_group_model')
+  # model = keras.Model(inputs=[inputs, addition_inputs], outputs=outputs, name='functional_group_model')
+  model = keras.Model(inputs=inputs, outputs=outputs, name='functional_group_model')
 
-  return model
 
-def train_model(model):
   model.compile(optimizer='adam',
             loss='binary_crossentropy',
             metrics=['binary_accuracy', 'Precision', 'Recall', 'TruePositives', 'FalseNegatives'])
 
-  history = model.fit([train_data, reduce_clarity(train_data)], train_label,
+  return model
+
+def train_model(model):
+  log_dir="./logs/fit/" + datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
+  tensorboard_callback = tf.keras.callbacks.TensorBoard(log_dir=log_dir, histogram_freq=1)
+  history = model.fit(train_data, train_label,
                     batch_size=64,
                     epochs=40,
                     # validation_split=0.2,
-                    validation_data=([validation_data, reduce_clarity(validation_data)], validation_label)
+                    callbacks=[tensorboard_callback],
+                    validation_data=(validation_data, validation_label)
                     )
   
   return model
 
 def evaludate_model(model):
-  test_result =  model.evaluate([test_data, reduce_clarity(test_data)],  test_label, verbose=2)
+  test_result =  model.evaluate(test_data, test_label, verbose=2)
   print('Test Result: ', test_result)
   test_num = len(test_data)
   samples = [random.randrange(0, test_num) for i in range(5)]
 
   print('\n# Generate predictions for 5 samples')
   prediction_data = np.array([test_data[i] for i in samples])
-  predictions = model.predict([prediction_data, reduce_clarity(prediction_data)])
+  predictions = model.predict(prediction_data)
   print(predictions)
   print([test_label[i] for i in samples])
+
+def save_model(model):
+  # model.save('./model/functional_group.h5')
+  model.save('./model/functional_group/1', save_format='tf')
 
 model = create_conv_model(addtion_num=addtion_num)
 
@@ -111,3 +122,4 @@ train_model(model)
 
 model.summary()
 evaludate_model(model)
+save_model(model)
